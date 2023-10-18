88b1899bca70e0ec2da462b6f97945be9d461c44
CodeNinjaWtobicoke <codeninjasetobicoke@outlook.com>
2023-10-18 17:58:30 -0400

    asdf
    sadfghgjkhg

---
+++ Assets/Scripts/TimersCountdown.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimersCountdown : MonoBehaviour
+{
+    public Text lapTime;
+    public Text startCountdown;
+
+    public float totalLapTime;
+    public float totalCountdownTime;
+
+    public CodeyMove A;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        totalLapTime -= Time.deltaTime;
+        totalCountdownTime -= Time.deltaTime;
+
+        lapTime.text = Mathf.Round(totalLapTime).ToString();
+        startCountdown.text = Mathf.Round(totalCountdownTime).ToString();
+
+        if (totalCountdownTime > 0)
+        {
+            
+        }
+    }
+}
