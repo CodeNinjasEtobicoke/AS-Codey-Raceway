0f79c07b46173788c4448dddc76e9518155bf398
CodeNinjasEtobicoke <codeninjasetobicoke@outlook.com>
2023-10-24 18:00:28 -0400

    asdfghjkl
    qasdcfvghjhg

--- Assets/Scripts/TimersCountdown.cs
+++ Assets/Scripts/TimersCountdown.cs
@@ -12,17 +12,29 @@ public class TimersCountdown : MonoBehaviour
     public float totalCountdownTime;
 
     public CodeyMove FriedChicken;
+    public GameObject testBlock;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //int testValue = 5;
+        //for (int i = 0; i < testValue; i++)
+        {
+            //for (int j = 0; j < testValue; j++)
+            {
+                //Vector3 stuff;
+                //stuff = transform.position;
+                //stuff.z += ((float)i * 1.5f);
+                //Instantiate(testBlock, stuff, Quaternion.identity);
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        totalLapTime -= Time.deltaTime;
-        totalCountdownTime -= Time.deltaTime;
+        //totalLapTime -= Time.deltaTime;
+        //totalCountdownTime -= Time.deltaTime;
 
         lapTime.text = Mathf.Round(totalLapTime).ToString();
         startCountdown.text = Mathf.Round(totalCountdownTime).ToString();
@@ -38,6 +50,13 @@ public class TimersCountdown : MonoBehaviour
         {
             startCountdown.text = "";
             totalLapTime -= Time.deltaTime;
+            lapTime.text = Mathf.Round(totalLapTime).ToString();
+            FriedChicken.Speed = 200f;
         }
+
+        if (totalLapTime < 0)
+        {
+            print("Time's Up!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        }   
     }
 }
